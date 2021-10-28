2353b4641697e8c5bc8e30f683b3807b1f9073ff
Aofei Sheng <aofei@aofeisheng.com>
2021-10-28 17:44:35 +0800

    test: no longer use github.com/stretchr/testify/assert

--- mimesniffer_test.go
+++ mimesniffer_test.go
@@ -1,38 +1,62 @@
 package mimesniffer
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
+import "testing"
 
 func TestRegister(t *testing.T) {
-	assert.Len(t, registeredSniffers, 0)
+	if l := len(registeredSniffers); l != 0 {
+		t.Errorf("got %d, want 0", l)
+	}
 
 	Register("", func([]byte) bool { return true })
-	assert.Len(t, registeredSniffers, 0)
+	if l := len(registeredSniffers); l != 0 {
+		t.Errorf("got %d, want 0", l)
+	}
 
 	Register("foobar", func([]byte) bool { return true })
-	assert.Len(t, registeredSniffers, 1)
+	if l := len(registeredSniffers); l != 1 {
+		t.Errorf("got %d, want 1", l)
+	}
 
 	Register("foo/bar", func([]byte) bool { return true })
-	assert.Len(t, registeredSniffers, 2)
+	if l := len(registeredSniffers); l != 2 {
+		t.Errorf("got %d, want 2", l)
+	}
 
 	Register("foo/bar; charset=utf8", func([]byte) bool { return true })
-	assert.Len(t, registeredSniffers, 3)
+	if l := len(registeredSniffers); l != 3 {
+		t.Errorf("got %d, want 3", l)
+	}
 }
 
 func TestSniff(t *testing.T) {
 	registeredSniffers = map[string]func([]byte) bool{}
 
-	assert.Equal(t, Sniff(nil), "application/octet-stream")
+	mimeType := Sniff(nil)
+	if want := "application/octet-stream"; mimeType != want {
+		t.Errorf("got %q, want %q", mimeType, want)
+	}
 
 	Register("foo/bar", func(b []byte) bool {
 		return len(b) > 0 && b[0] == 0x00
 	})
 
-	assert.Equal(t, Sniff([]byte{0x00}), "foo/bar")
-	assert.Equal(t, Sniff([]byte{0x01}), "application/octet-stream")
-	assert.Equal(t, Sniff([]byte{0xff, 0xf1}), "audio/aac")
-	assert.Equal(t, Sniff([]byte("foobar")), "text/plain; charset=utf-8")
+	mimeType = Sniff([]byte{0x00})
+	if want := "foo/bar"; mimeType != want {
+		t.Errorf("got %q, want %q", mimeType, want)
+	}
+
+	mimeType = Sniff([]byte{0x01})
+	if want := "application/octet-stream"; mimeType != want {
+		t.Errorf("got %q, want %q", mimeType, want)
+	}
+
+	mimeType = Sniff([]byte{0xff, 0xf1})
+	if want := "audio/aac"; mimeType != want {
+		t.Errorf("got %q, want %q", mimeType, want)
+	}
+
+	mimeType = Sniff([]byte("foobar"))
+	if want := "text/plain; charset=utf-8"; mimeType != want {
+		t.Errorf("got %q, want %q", mimeType, want)
+	}
 }
